a922f5ef786ee67d9a19eef40b69bd1a3d100caf
srijansil2006 <srijankhamrai2006@gmail.com>
2026-07-05 19:12:08 +0530

    1458-SORT-INTEGERS-BY-THE-NUMBER-OF-1-BITS Stats: Time: 0 ms (100%), Space: 14.2 MB (51.88%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    static bool cmp(int a, int b){
-        int cntA = __builtin_popcount(a);
-        int cntB = __builtin_popcount(b);
-        if (cntA != cntB) return cntA < cntB;
-        return a < b;
-    }
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), cmp);
+        sort(arr.begin(), arr.end(), [](int a, int b){
+            int cntA = __builtin_popcount(a);
+            int cntB = __builtin_popcount(b);
+            if(cntA!=cntB) return (cntA<cntB);
+            else return a<b;
+        });
         return arr;
     }
 };
