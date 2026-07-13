3016601b775cb59bb8bf3148fac814b250a92fe0
Srijansil Khamrai <srijankhamrai2006@gmail.com>
2026-07-13 09:31:10 +0530

    1212-SEQUENTIAL-DIGITS Stats: Time: 0 ms (100%), Space: 8.5 MB (18.24%) - LeetHub

--- 1212-sequential-digits/1212-sequential-digits.cpp
+++ 1212-sequential-digits/1212-sequential-digits.cpp
@@ -3,20 +3,9 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int>arr;
         string str = "123456789";
-        int dl = 0, dh = 0;
-        int num = low;
-        while(num>0){
-            dl++;
-            num /= 10;
-        }
-        num = high;
-        while(num>0){
-            dh++;
-            num /= 10;
-        }
-        for(int i=dl; i<=dh; i++){
+        for(int i=2; i<=9; i++){
             int l = 0;
-            while(l+i<10){
+            while(l+i <=9){
                 string s = str.substr(l, i);
                 int n = std::stoi(s);
                 if(n>=low && n<=high) arr.push_back(n);
