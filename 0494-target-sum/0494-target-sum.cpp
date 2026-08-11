2169173afe91a0709f7361adb70bbe5e109679af
Srijansil Khamrai <srijankhamrai2006@gmail.com>
2026-08-11 13:55:39 +0530

    0494-TARGET-SUM Stats: Time: 0 ms (100%), Space: 12.3 MB (71.5%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,14 +6,13 @@ public:
         for(int i=0; i<n; i++) tsm += nums[i];
         if(abs(t)>tsm || (t+tsm)&1) return 0;
         int tar = (t+tsm)/2;
-        vector<vector<int>>dp(n+1, vector<int>(tar+1, 0));
-        dp[0][0] = 1;
-        for(int i=1; i<=n; i++){
-            for(int s=0; s<=tar; s++){
-                dp[i][s] += dp[i-1][s];
-                if(nums[i-1]<=s) dp[i][s] += dp[i-1][s-nums[i-1]];
+        vector<int>dp(tar+1, 0);
+        dp[0] = 1;
+        for(int i=0; i<n; i++){
+            for(int s=tar; s>=nums[i]; s--){
+                dp[s] += dp[s-nums[i]];
             }
         }
-        return dp[n][tar];
+        return dp[tar];
     }
 };
